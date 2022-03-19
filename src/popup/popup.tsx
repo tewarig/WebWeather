37080af6d9afd50d9dc157d435ae44cbda231589
tewarig <gauravtewari111@gmail.com>
2022-03-19 22:33:29 +0530

    feat: inital ui created

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,17 +1,42 @@
-import React, { useEffect , useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import WeatherCard  from "./weatherCard/weatherCard";
+import { InputBase, IconButton, Paper, Grid, Box } from "@mui/material";
+import { Add as AddIcon } from "@mui/icons-material";
+import WeatherCard from "./weatherCard/weatherCard";
 import "./popup.css";
 
-
 const App: React.FC<{}> = () => {
-  
+  const [Cities, setCities] = useState<String[]>([
+    "Haldwani",
+    "Dehradun",
+    "New York",
+  ]);
+  const [citiesInput, setCitiesInput] = useState<String>("");
+  console.log(citiesInput);
 
   return (
-    <div>
-      <WeatherCard city="Haldwani"/>
-     
-    </div>
+    <Box mx="8px" my="16px">
+      <Grid container>
+        <Grid item>
+          <Paper>
+            <Box px="15px" py="5px">
+              {" "}
+              <InputBase
+                placeholder="Add City Name"
+                value={citiesInput}
+                onChange={(event) => setCitiesInput(event.target.value)}
+              />
+              <IconButton>
+                <AddIcon />
+              </IconButton>
+            </Box>
+          </Paper>
+          {Cities.map((cityName, index) => (
+            <WeatherCard city={cityName} key={index} />
+          ))}
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
--- src/popup/weatherCard/weatherCard.tsx
+++ src/popup/weatherCard/weatherCard.tsx
@@ -1,20 +1,58 @@
 import React, { useState, useEffect } from "react";
-import { featherWeatherDetails } from "../../utils/api";
-
+import { Card, CardContent, Typography, Box } from "@mui/material";
+import { featherWeatherDetails, OpenWeatherApiData } from "../../utils/api";
 
 const WeatherCard = ({ city }) => {
+  const [WeatherData, SetWeatherData] = useState<OpenWeatherApiData | null>(
+    null
+  );
+  const [error, setError] = useState<boolean>(false);
   useEffect(() => {
-    featherWeatherDetails("Haldwani")
+    featherWeatherDetails(city)
       .then((data) => {
         console.log("Data mil gaya bhai");
         console.log(data.main.temp);
+        SetWeatherData(data);
       })
       .catch((error) => {
         console.log("error");
         console.log(error);
+        setError(true);
       });
   }, [city]);
-  return <div>{city} </div>;
+  if (!WeatherData) {
+    return <>Loading....</>;
+  }
+  if (error) {
+    return (
+      <Box mx={"4px"} my={"16px"}>
+        <Card>
+          {" "}
+          <CardContent>
+            {" "}
+            <Typography variant="body1"> Error in getting Data</Typography>{" "}
+          </CardContent>{" "}
+        </Card>{" "}
+      </Box>
+    );
+  }
+
+  return (
+    <Box mx={"4px"} my={"16px"}>
+      <Card>
+        <CardContent>
+          <Typography variant="h5">{WeatherData.name} </Typography>
+          <Typography variant="body1">
+            {" "}
+            {Math.round(WeatherData.main.temp)}
+          </Typography>
+          <Typography variant="body1">
+            Feels Like: {Math.round(WeatherData.main.feels_like)}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  );
 };
 
 export default WeatherCard;
